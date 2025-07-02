a27f079da2ef2a682a558e8fe2b67ae001096dac
SabihaChowdhury789 <Sabihahaquechowdhury@gmail.com>
2025-07-03 05:45:52 +0600

    Fruits

---
+++ hmtl.lllll/index.js
@@ -0,0 +1,12 @@
+let fruits=["apple", 5,"#"];
+
+console.log(fruits);
+
+if(fruits[0]=="apple"){
+    alert("exist");
+}
+else{
+    alert("nonexistent");
+}
+//if(fruits[1]=="apple"){
+//if(fruits.include=="apple"){
